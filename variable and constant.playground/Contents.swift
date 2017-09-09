02a5efff66632420553849800540c46c99013248
prachuabwongsa <ppectwe@gmail.com>
2017-09-09 14:37:27 +0700

    Array

---
+++ variable and constant.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//การประกาศตัวแปร บน ภาษา swift
+//Implicit
+
+var namestring = "Master Ung"
+
+namestring = "Horse"
+
+
+//constant
+
+let surnamestring = "ABCD"
+surnamestring = "EFGH"
+//surnamestring = "EFGH" // error เพราะ let แก้ไขไม่ได้
+
+//Print
+print("name ==> \(namesting)"
+
+let number1AInt =1
+let nuber2AInt: int = 10 //นี่คือการประกาศแบบ กำหนด Type Data
+
+let answer = number1AInt + number2Aint
+print("answer ==> \(answer)")
+
+
+
+
+
+
+
+
+
+
+
+
+
