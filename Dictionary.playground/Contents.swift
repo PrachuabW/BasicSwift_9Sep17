ef2458dfe9843d73e13f59bfbf15975f34d88991
prachuabwongsa <ppectwe@gmail.com>
2017-09-09 14:55:46 +0700

    Dic

---
+++ Dictionary.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var nameDictionary = ["Father": "น่ี่คือ พ่อ", "Mother": "นี่คือ แม่","son" : "นี่คือ ลูก"]
+
+print("ขนาดของ Dictionary ==>\(nameDictionary.count)")
+
+print("nameDictionary ที่มี ค่า key คือ mother ==>\(String(describing: nameDictionary["mother"]))")
+
+print("nameDictionary ของ father \(nameDictionary["father]!)")
+
+
