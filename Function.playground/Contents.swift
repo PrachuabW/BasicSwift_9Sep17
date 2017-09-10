eae0c009737489229efc1d43fcfb29995c0d601f
prachuabwongsa <ppectwe@gmail.com>
2017-09-10 10:47:44 +0700

    class

---
+++ Function.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// How to use Class and Function
+
+
+// Create Class 
+class MyClass {
+    //In MyClass
+
+    //1 Share Variable and Constant
+    var nameString: String = "Master UNG"
+    var ungAInt: Int = 100
+    var ungADouble: Double = 3.1416
+    var ungBool: Bool = true
+    
+
+}   // MyClass
+
+// Outside MyClass
+// Extend MyClass
+
+var ungClass = MyClass{}
+
+
+
+
+//print("Name ==>\(nameString)")
+
+
